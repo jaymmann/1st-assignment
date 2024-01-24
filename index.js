5076618c0038a5de7af5fe4ff1d53df4df9057a9
Jayden Manning <jaymmann@iu.edu>
2024-01-24 21:20:04 +0000

    Initial Commit

---
+++ index.js
@@ -0,0 +1,7 @@
+// index.js
+const Distance = require('./distance');
+
+Distance.calculate('feet', 30);
+Distance.calculate('meters', 10);
+Distance.calculate('miles', 5);
+Distance.calculate('km', 8);
